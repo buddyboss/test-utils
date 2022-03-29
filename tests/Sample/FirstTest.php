8fd1dc5b42cd3757dc7c4f860c0e601557828011
Ketan Chawda <chawda.ketan.a@gmail.com>
2022-03-28 18:10:30 -0700

    Create FirstTest.php

---
+++ tests/Sample/FirstTest.php
@@ -0,0 +1,13 @@
+<?php
+namespace Sample;
+
+class FirstTest extends \WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
