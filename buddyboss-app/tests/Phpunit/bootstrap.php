db15ccaf8b5490cc3aa8f897c07a7dbd17da2139
Ketan Chawda <chawda.ketan.a@gmail.com>
2022-04-11 11:29:11 -0700

    Files reorganized

--- buddyboss-app/tests/Phpunit/bootstrap.php
+++ buddyboss-app/tests/Phpunit/bootstrap.php
@@ -26,3 +26,6 @@ tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
 // Start up the WP testing environment.
 require $_tests_dir . '/includes/bootstrap.php';
+
+require( dirname( __FILE__ ) . '/includes/define-constants.php' );
+
